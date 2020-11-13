a1c2553e94bcfa47a9c033b3e3dbd1dc9e97e779
晨阳 <892474885@qq.com>
2020-11-13 17:54:54 +0800

    123

--- scripts/verify-commit.js
+++ scripts/verify-commit.js
@@ -4,6 +4,7 @@
 // const msg = fs.readFileSync(msgPath, 'utf-8').trim();
 // const commitRE = /^(feat|fix|docs|style|refactor|perf|test|workflow|build|ci|chore|release|workflow)(\(.+\))?(:|：)(\s)?.{1,50}/;
 
+
 // if (!commitRE.test(msg)) {
     console.log('\x1B[31m%s\x1B[0m', `
         不合法的commit提交格式

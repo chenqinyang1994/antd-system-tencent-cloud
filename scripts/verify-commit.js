735d64acfdfc956859abef13b05cf98abe671cfc
晨阳 <892474885@qq.com>
2020-11-13 11:51:55 +0800

    chore(common):验证husky的输出

---
+++ scripts/verify-commit.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const msgPath = process.env.HUSKY_GIT_PARAMS;
+
+console.log('====================================');
+console.log('msgPath', msgPath);
+console.log('====================================');

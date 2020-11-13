7d5340ce64d30be9fbcfc04ab5020e4a18d3f062
晨阳 <892474885@qq.com>
2020-11-13 11:57:43 +0800

    chore(common):获取husky的commit输出

--- scripts/verify-commit.js
+++ scripts/verify-commit.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const msgPath = process.env.HUSKY_GIT_PARAMS;
 
+const msg = fs.readFileSync(msgPath, 'utf-8');
+
 console.log('====================================');
-console.log('msgPath', msgPath);
+console.log('msg', msg);
 console.log('====================================');
